f71c07ffff89808027ae388b56d3fa348b64f6ea
BlackbirdBlina <sabrinapop_rn@hotmail.com>
2025-07-05 16:31:55 -0300

    Excluindo arquivo hpp desnecessário

--- includes/corretor.hpp
+++ includes/corretor.hpp
@@ -6,16 +6,24 @@ using namespace std;
 static int nextIdCorretor = 1;
 class Corretor{
     private:
+    int id;
+    string nome;
+    string telefone;
     bool avaliador;
     double lat;
     double lng;
 
     public:
     Corretor(string nome, float salarioBase, int id, int quantidadeDeProjetos);
+    void setId(int id);
+    void setNome(string nome);
+    void setTelefone(string telefone);
 
     ~Corretor() = default;
 
     int getId();
+    string getNome();
+    string getTelefone();
 
 };
 
