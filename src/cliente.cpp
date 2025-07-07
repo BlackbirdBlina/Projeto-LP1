d11a5d2db6b3a94e1f1227b635df85b698303524
BlackbirdBlina <sabrinapop_rn@hotmail.com>
2025-07-07 14:50:14 -0300

    Adicionando implementação para leitura de dados

--- src/cliente.cpp
+++ src/cliente.cpp
@@ -1,9 +1,20 @@
-#include "cliente.hpp"
+#include "cliente.h"
+#include <iostream>
 using namespace std;
 
 int Cliente::nextId = 1;
 
 Cliente::Cliente(string nome, string telefone):
     id(nextId++), nome(nome), telefone(telefone){
+}
 
+vector<Cliente> lerCliente(int quantidade){
+    vector<Cliente> clientes;
+    for (int i = 0; i < quantidade; ++i) {
+        string tel, nome;
+        cin >> tel;
+        getline(cin >> ws, nome);
+        clientes.emplace_back(tel, nome);
+    }
+    return clientes;
 }
--- src/corretor.cpp
+++ src/corretor.cpp
@@ -1,4 +1,6 @@
-#include "corretor.hpp"
+#include "corretor.h"
+#include <iostream>
+#include <vector>
 using namespace std;
 
 int Corretor::nextId = 1;
@@ -6,4 +8,20 @@ int Corretor::nextId = 1;
 Corretor::Corretor(string nome, string telefone, bool avaliador, double lat, double lng):
     id(nextId++), nome(nome), telefone(telefone), avaliador(avaliador), lat(lat), lng(lng){
 
+}
+
+vector<Corretor> lerCorretor(int quantidade, vector<Corretor&> &avaliadores){
+    vector<Corretor> corretores;
+    for (int i = 0; i < quantidade; ++i) {
+        string tel, nome;
+        int avaliador;
+        double lat, lng;
+        cin >> tel >> avaliador >> lat >> lng;
+        getline(cin >> ws, nome);
+        corretores.emplace_back(tel, avaliador, lat, lng, nome);
+        if (avaliador){
+            avaliadores.push_back(corretores.back());
+        }
+    }
+    return corretores;
 }
